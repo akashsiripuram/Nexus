fadbe51c0a7581088e788dae9040240c9995a98d
Akash Siripuram <144911464+akashsiripuram@users.noreply.github.com>
2025-08-24 01:12:28 +0530

    added files

--- backend/utils/connection.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-export default function connectDB(){
-    mongoose.connect(process.env.MONGO_URI)
-    .then(()=>console.log("connected to db"))
-    .catch((err)=>console.log("Error connecting to DB"))
-}
